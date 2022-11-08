e1eef2159e5488a40675dfb7fb3942ad0f26662c
Wyatt Walker <rwyattwalker@gmail.com>
2022-11-08 12:13:29 -0600

    Move Initial Files to TS

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './ThemeContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'):HTMLElement|null);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
